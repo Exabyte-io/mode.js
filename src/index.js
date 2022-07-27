08273f7d5caf55e584f3521707fd41c928b24cc1
tjduigna <thomas.j.duignan@gmail.com>
2022-07-27 14:02:40 -0400

    feat: export tree

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
+import * as context from "./context";
 import { Method } from "./method";
-import { Model } from "./model";
 import { MethodFactory } from "./methods/factory";
+import { Model } from "./model";
 import { ModelFactory } from "./models/factory";
-import * as context from "./context"
+import * as tree from "./tree";
 
-export {
-    Method,
-    Model,
-    MethodFactory,
-    ModelFactory,
-    context
-}
+export { Method, Model, MethodFactory, ModelFactory, context, tree };
