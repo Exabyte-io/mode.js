8476a0163934d5995c54a2f6353e0e4c6c50af80
Alexander Zech <alexzech@gmx.de>
2022-09-28 19:15:12 -0700

    chore: add container functionality and state management

--- src/compound_model.js
+++ src/compound_model.js
@@ -1,13 +1,18 @@
 import { InMemoryEntity } from "@exabyte-io/code.js/dist/entity";
 import { setNextLinks, setUnitsHead } from "@exabyte-io/code.js/dist/utils";
 
+import { UnitModel } from "./unit_model";
+// import { UnitModelFactory } from "./unit_model_factory";
 import { buildModelPath } from "./utils";
 
 export class Model extends InMemoryEntity {
-    constructor(config) {
-        super(config);
+    // static UnitModelFactory = UnitModelFactory;
+    static UnitModelCls = UnitModel;
 
-        this.units = [];
+    constructor({ ...config } = {}) {
+        super(config);
+        this._units = Model.instantiateUnits(config.units);
+        this.setNextLinks().setUnitsHead().syncToProp();
     }
 
     get name() {
@@ -18,53 +23,119 @@ export class Model extends InMemoryEntity {
         return this.prop("slug");
     }
 
+    static isUnitModelArray(unitInstances) {
+        return (
+            Array.isArray(unitInstances) &&
+            unitInstances.every((u) => u instanceof Model.UnitModelCls)
+        );
+    }
+
+    static instantiateUnits(unitConfigs) {
+        if (!Array.isArray(unitConfigs) || !unitConfigs) return [];
+        return unitConfigs.map((config) => new Model.UnitModelCls(config));
+    }
+
+    get units() {
+        return this._units;
+    }
+
+    /**
+     *  Set units and update `this._json`.
+     */
+    set units(unitInstances) {
+        const valid = Model.isUnitModelArray(unitInstances) || !unitInstances.length;
+        if (valid) {
+            this._units = unitInstances;
+            this.setNextLinks().setUnitsHead().syncToProp();
+        }
+    }
+
+    setUnitModels(unitInstances) {
+        this.units = unitInstances;
+        return this;
+    }
+
     setNextLinks() {
-        this.units = setNextLinks(this.units);
+        if (this._units.length > 0) {
+            this._units = setNextLinks(this._units);
+        }
         return this;
     }
 
     setUnitsHead() {
-        this.units = setUnitsHead(this.units);
+        if (this._units.length > 0) {
+            this._units = setUnitsHead(this._units);
+        }
         return this;
     }
 
     addUnitModel(unitModel, index = -1) {
-        if (this.units.length === 0) {
-            this.units = [unitModel];
+        if (this._units.length === 0) {
+            this._units = [unitModel];
         } else if (index === 0) {
-            unitModel.next = this.units[0].flowchartId;
-            this.units.unshift(unitModel);
-        } else if (index > 0 && index < this.units.length) {
-            this.units.splice(index, 0, unitModel);
+            unitModel.next = this._units[0].flowchartId;
+            this._units.unshift(unitModel);
+        } else if (index > 0 && index < this._units.length) {
+            this._units.splice(index, 0, unitModel);
             this.setNextLinks();
         } else {
-            this.units.push(unitModel);
+            this._units.push(unitModel);
             this.setNextLinks();
         }
-        this.setUnitsHead().updateModelPath();
+        return this.setUnitsHead().syncToProp();
+    }
+
+    updateUnitModelByIndex(unitModel, index) {
+        this._units[index] = unitModel;
+        return this.setNextLinks().setUnitsHead().syncToProp();
+    }
+
+    updateUnitModelById(unitModel, flowchartId) {
+        const idx = this.units.findIndex((um) => um.flowchartId === flowchartId);
+        if (idx) {
+            this.updateUnitModelByIndex(unitModel, idx);
+        }
+        return this;
     }
 
     removeUnitModelById(flowchartId) {
         this.units = this.units.filter((um) => um.flowchartId !== flowchartId);
-        this.setNextLinks().setUnitsHead().updateModelPath();
+        return this;
     }
 
     removeUnitModelByIndex(index) {
-        this.units.splice(index, 1);
-        this.setNextLinks().setUnitsHead().updateModelPath();
+        this._units.splice(index, 1);
+        return this.setNextLinks().setUnitsHead().syncToProp();
     }
 
     get modelPath() {
         return this.prop("modelPath");
     }
 
-    updateModelPath() {
-        const modelPath = buildModelPath(this.units);
-        this.setProp("modelPath", modelPath);
+    syncModelPath() {
+        this.setProp("modelPath", buildModelPath(this.units));
         return this;
     }
 
-    get graph() {
+    syncUnitModels() {
+        this.setProp(
+            "units",
+            this.units.map((um) => um.toJSON()),
+        );
+        return this;
+    }
+
+    syncGraph() {
+        this.setProp("graph", this.buildGraph);
+        return this;
+    }
+
+    syncToProp() {
+        this.syncModelPath().syncUnitModels().syncGraph();
+        return this;
+    }
+
+    buildGraph() {
         if (this.units.length === 0) return [];
 
         return this.units.map((um) => ({
@@ -74,6 +145,10 @@ export class Model extends InMemoryEntity {
             ...(um.next ? { next: um.next } : {}),
             ...(um.workflowUnitId ? { workflowUnitId: um.workflowUnitId } : {}),
         }));
+    }
+
+    get graph() {
+        return this.prop("graph");
     }
 
     toJSON() {
