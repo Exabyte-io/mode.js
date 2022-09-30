7ab92e7964f9f7958df12f5b88e14630b8c5a642
Alexander Zech <alexzech@gmx.de>
2022-09-30 11:10:49 -0700

    chore: use unit model factory in compound model

--- src/compound_model.js
+++ src/compound_model.js
@@ -2,11 +2,12 @@ import { InMemoryEntity } from "@exabyte-io/code.js/dist/entity";
 import { setNextLinks, setUnitsHead } from "@exabyte-io/code.js/dist/utils";
 
 import { UnitModel } from "./unit_model";
-// import { UnitModelFactory } from "./unit_model_factory";
+import { UnitModelFactory } from "./unit_model_factory";
 import { buildModelPath } from "./utils";
 
 export class Model extends InMemoryEntity {
-    // static UnitModelFactory = UnitModelFactory;
+    static UnitModelFactory = UnitModelFactory;
+
     static UnitModelCls = UnitModel;
 
     constructor({ ...config } = {}) {
@@ -32,9 +33,12 @@ export class Model extends InMemoryEntity {
 
     static instantiateUnits(unitConfigs) {
         if (!Array.isArray(unitConfigs) || !unitConfigs) return [];
-        return unitConfigs.map((config) => new Model.UnitModelCls(config));
+        return unitConfigs.map((config) => Model.UnitModelFactory.create(config));
     }
 
+    /**
+     *  Get list of unit model _instances_
+     */
     get units() {
         return this._units;
     }
