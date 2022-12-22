f94b1f7549e2e8be54bac39d525aa01c10c511a0
Alexander Zech <alexzech@gmx.de>
2022-12-22 19:12:40 +0100

    chore: set next links in constructor

--- src/compound_model.js
+++ src/compound_model.js
@@ -3,6 +3,7 @@ import {
     InMemoryEntity,
     NamedEntityMixin,
 } from "@exabyte-io/code.js/dist/entity";
+import { setNextLinks, setUnitsHead } from "@exabyte-io/code.js/dist/utils";
 import { mix } from "mixwith";
 
 import { UnitModel } from "./unit_model";
@@ -29,7 +30,8 @@ export class Model extends mix(InMemoryEntity).with(NamedEntityMixin, FlowchartE
 
     static instantiateUnits(unitConfigs) {
         if (!Array.isArray(unitConfigs) || !unitConfigs) return [];
-        return unitConfigs.map((config) => Model.UnitModelFactory.create(config));
+        const units = unitConfigs.map((config) => Model.UnitModelFactory.create(config));
+        return setNextLinks(setUnitsHead(units));
     }
 
     buildModelPath() {
@@ -44,6 +46,10 @@ export class Model extends mix(InMemoryEntity).with(NamedEntityMixin, FlowchartE
             ...(um.next ? { next: um.next } : {}),
             ...(um.workflowUnitId ? { workflowUnitId: um.workflowUnitId } : {}),
         }));
+    }
+
+    get methods() {
+        return this.units.map((unit) => unit.method);
     }
 
     toJSON() {
