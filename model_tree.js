a55ce662899a59f39c3befd596ece6140da22afb
Alexander Zech <alexzech@gmx.de>
2022-12-23 15:59:38 +0100

    chore: add common mGGA and hybrid functionals

--- model_tree.js
+++ model_tree.js
@@ -11,11 +11,7 @@ module.exports = {
                     path: "/dft/gga",
                     data: {
                         isDefault: true,
-                        subtype: {
-                            slug: "gga",
-                            name: "Generalized Gradient Approximation",
-                            default: "gga",
-                        },
+                        subtype: { slug: "gga", name: "Generalized Gradient Approximation" },
                     },
                     children: [
                         [
@@ -67,6 +63,84 @@ module.exports = {
                 },
                 {
                     parentPath: "/dft",
+                    path: "/dft/hybrid",
+                    data: { subtype: { slug: "hybrid", name: "Hybrid Functional" } },
+                    children: [
+                        [
+                            {
+                                path: "/dft/hybrid/b3lyp",
+                                data: {
+                                    name: "B3LYP",
+                                    slug: "b3lyp",
+                                    components: [
+                                        { slug: "exact-exchange", fraction: 0.2 },
+                                        {
+                                            slug: "slater",
+                                            fraction: 0.08,
+                                        },
+                                        { slug: "b88", fraction: 0.72 },
+                                        { slug: "vwn1rpa", fraction: 0.19 },
+                                        {
+                                            slug: "lyp",
+                                            fraction: 0.81,
+                                        },
+                                    ],
+                                },
+                                parentPath: "/dft/hybrid",
+                            },
+                            {
+                                path: "/dft/hybrid/pbe0",
+                                data: {
+                                    name: "PBE0",
+                                    slug: "pbe0",
+                                    components: [
+                                        { slug: "exact-exchange", fraction: 0.25 },
+                                        {
+                                            slug: "pbe-x",
+                                            fraction: 0.75,
+                                        },
+                                        { slug: "pbe-c", fraction: 1 },
+                                    ],
+                                },
+                                parentPath: "/dft/hybrid",
+                            },
+                            {
+                                path: "/dft/hybrid/hse06",
+                                data: {
+                                    name: "HSE06",
+                                    slug: "hse06",
+                                    attenuation: 0.11,
+                                    components: [
+                                        {
+                                            slug: "exact-exchange",
+                                            range: "short-range",
+                                            fraction: 0.25,
+                                        },
+                                        {
+                                            slug: "exact-exchange",
+                                            range: "long-range",
+                                            fraction: 0,
+                                        },
+                                        {
+                                            slug: "wpbe-x",
+                                            range: "short-range",
+                                            fraction: -0.25,
+                                        },
+                                        { slug: "wpbe-x", range: "long-range", fraction: 0 },
+                                        {
+                                            slug: "pbe-x",
+                                            fraction: 1,
+                                        },
+                                        { slug: "pbe-c", fraction: 1 },
+                                    ],
+                                },
+                                parentPath: "/dft/hybrid",
+                            },
+                        ],
+                    ],
+                },
+                {
+                    parentPath: "/dft",
                     path: "/dft/lda",
                     data: { subtype: { slug: "lda", name: "Local Density Approximation" } },
                     children: [
@@ -106,6 +180,53 @@ module.exports = {
                                     ],
                                 },
                                 parentPath: "/dft/lda",
+                            },
+                        ],
+                    ],
+                },
+                {
+                    parentPath: "/dft",
+                    path: "/dft/mgga",
+                    data: {
+                        subtype: { slug: "mgga", name: "Meta Generalized Gradient Approximation" },
+                    },
+                    children: [
+                        [
+                            {
+                                path: "/dft/mgga/scan",
+                                data: {
+                                    name: "SCAN",
+                                    slug: "scan",
+                                    components: [
+                                        { slug: "scan-x", fraction: 1 },
+                                        { slug: "scan-c", fraction: 1 },
+                                    ],
+                                },
+                                parentPath: "/dft/mgga",
+                            },
+                            {
+                                path: "/dft/mgga/tpss",
+                                data: {
+                                    name: "TPSS",
+                                    slug: "tpss",
+                                    components: [
+                                        { slug: "tpss-x", fraction: 1 },
+                                        { slug: "tpss-c", fraction: 1 },
+                                    ],
+                                },
+                                parentPath: "/dft/mgga",
+                            },
+                            {
+                                path: "/dft/mgga/m06l",
+                                data: {
+                                    name: "M06-L",
+                                    slug: "m06-l",
+                                    components: [
+                                        { slug: "m06-l-x", fraction: 1 },
+                                        { slug: "m06-l-c", fraction: 1 },
+                                    ],
+                                },
+                                parentPath: "/dft/mgga",
                             },
                         ],
                     ],
