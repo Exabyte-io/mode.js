e88addc17f7d14d090baa35fc4153b4ef91959d6
Alexander Zech <alexzech@gmx.de>
2023-02-02 13:21:18 -0800

    chore: export compound model in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import { Model } from "./compound_model";
 import * as default_methods from "./default_methods";
 import * as default_models from "./default_models";
 import { Method } from "./method";
 import { MethodFactory } from "./methods/factory";
 import { PseudopotentialMethod } from "./methods/pseudopotential";
-import { Model } from "./model";
 import { DFTModel } from "./models/dft";
 // TODO : can replace these with subpackages in exports in package.json
 import * as tree from "./tree";
