19163f606853accd3b1404ea21b4d8ca16d5472d
Alexander Zech <alexzech@gmx.de>
2023-04-28 13:55:49 -0700

    chore: generate name based on template

--- build_models.mjs
+++ build_models.mjs
@@ -1,11 +1,43 @@
 import fs from "fs";
 import yaml from "js-yaml";
+import lodash from "lodash";
 import { allYAMLSchemas } from "@exabyte-io/code.js/dist/utils";
+
+/**
+ * Render name template based on config.
+ * Use substitution map to replace a config value with a more readable variant.
+ * @param {string} template - Template for the name property
+ * @param {Object} data - Entity config
+ * @param {Object} substitutionMap - Maps object value to human-readable string
+ * @return {string}
+ */
+function generateName(template, data, substitutionMap = {}) {
+    // Regular expression to match the template variables, e.g `$job.workflow`
+    const regex = /\$(\w+(\.\w+)*)/g;
+
+    return template.replace(regex, (match, objPath) => {
+        const value = lodash.get(data, objPath)
+
+        if (substitutionMap[value]) {
+            return substitutionMap[value];
+        }
+
+        return value || "";
+    });
+}
+
 
 try {
     const testContent = fs.readFileSync("./assets/test.yml", "utf-8");
-    const parsed = yaml.load(testContent, { schema: allYAMLSchemas });
-    console.log(JSON.stringify(parsed, null, 4));
+    const parsed = yaml.load(testContent, {schema: allYAMLSchemas});
+
+    // Assume either array of configs or object with array of configs as values
+    let configs = lodash.isPlainObject(parsed) ? Object.values(parsed).flat() : parsed;
+    configs.forEach((config) => {
+        config.name = generateName(config.name?.template, config, config.name?.substitutions);
+    })
+
+    console.log(JSON.stringify(configs, null, 4));
 } catch (e) {
     console.error("Error parsing YAML:", e);
 }
