c6b8ddbaa3d4c8677146e9469bcea6ea980ba80e
Alexander Zech <alexzech@gmx.de>
2023-05-17 15:53:47 -0700

    chore: adjust build script to use all assets

--- build_models.mjs
+++ build_models.mjs
@@ -1,30 +1,55 @@
+import { allYAMLSchemas } from "@exabyte-io/code.js/dist/utils";
+import Ajv from "ajv";
 import fs from "fs";
 import yaml from "js-yaml";
 import lodash from "lodash";
-import { allYAMLSchemas } from "@exabyte-io/code.js/dist/utils";
-import Ajv from "ajv";
+import path from "path";
 
-function createModelConfigs(assetPath) {
-    try {
-        const testContent = fs.readFileSync(assetPath, "utf-8");
-        const parsed = yaml.load(testContent, { schema: allYAMLSchemas });
+const MODEL_ASSETS_PATH = "assets/models"
 
-        // Assume either array of configs or object with array of configs as values
-        let configs = lodash.isPlainObject(parsed) ? Object.values(parsed).flat() : parsed;
+function getAssetFiles(currentPath, assetExtension = ".yml", resolvePath = true) {
+    const fileNames = fs
+        .readdirSync(currentPath)
+        .filter((dirItem) => path.extname(dirItem) === assetExtension);
+    if (resolvePath) return fileNames.map((fileName) => path.resolve(currentPath, fileName));
+    return fileNames;
+}
 
-        configs.forEach((config) => {
-            const ajv = new Ajv({allErrors: true, verbose: true});
-            const validate = ajv.compile(config.schema);
-            console.log(`${config.schema.schemaId}: ${validate(config)}`);
-            console.error(validate.errors);
-            delete config.schema;
-        });
+function validateConfig(config, debug = false) {
+    const ajv = new Ajv({allErrors: true, verbose: true});
+    const validate = ajv.compile(config.schema);
+    if (debug) console.log(`${config.schema.schemaId}: ${validate(config)}`);
 
-        console.log(JSON.stringify(configs, null, 4));
-    } catch (e) {
-        console.error("Error parsing YAML:", e);
+    if (validate.errors) {
+        console.error("Validation errors:", validate.errors);
+        throw new Error(`Validation failed for ${config.schema.schemaId}`)
     }
+}
+
+function createModelConfigs(assetPath, debug = false) {
+    const testContent = fs.readFileSync(assetPath, "utf-8");
+    const parsed = yaml.load(testContent, {schema: allYAMLSchemas});
+
+    // Assume either array of configs or object with array of configs as values
+    let configs = lodash.isPlainObject(parsed) ? Object.values(parsed).flat() : parsed.flat();
+
+    configs.forEach((config) => {
+        validateConfig(config, false);
+        delete config.schema;
+    });
+
+    if (debug) configs.forEach((c) => console.log(`Creating config: ${c.name}`));
+    return configs;
+
 
 }
 
-createModelConfigs("./assets/test.yml")
+// Build and write model configs
+try {
+    const modelAssetFiles = getAssetFiles(MODEL_ASSETS_PATH, ".yml");
+    const modelConfigs = modelAssetFiles.flatMap((asset) => createModelConfigs(asset, true));
+    fs.writeFileSync("./model_list.js", `module.exports = ${JSON.stringify(modelConfigs)}`, "utf8");
+} catch (e) {
+    console.error(e);
+}
+
