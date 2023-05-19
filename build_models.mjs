e95860c4795c88ca161899af812014b470df9678
Alexander Zech <alexzech@gmx.de>
2023-05-18 19:57:38 -0700

    chore: move asset creation to single script

--- build_models.mjs
+++ build_models.mjs
@@ -6,6 +6,7 @@ import lodash from "lodash";
 import path from "path";
 
 const MODEL_ASSETS_PATH = "assets/models"
+const METHOD_ASSETS_PATH = "assets/methods"
 
 function getAssetFiles(currentPath, assetExtension = ".yml", resolvePath = true) {
     const fileNames = fs
@@ -38,13 +39,29 @@ function createModelConfigs(assetPath, debug = false) {
         delete config.schema;
     });
 
-    if (debug) configs.forEach((c) => console.log(`Creating config: ${c.name}`));
+    if (debug) configs.forEach((c) => console.log(`Creating model config: ${c.name}`));
     return configs;
-
-
 }
 
-// Build and write model configs
+function createMethodConfigs(assetPath, debug = false) {
+    const testContent = fs.readFileSync(assetPath, "utf-8");
+    const parsed = yaml.load(testContent, { schema: allYAMLSchemas });
+
+    // Iterate over groups of configs and set config-based values
+    parsed.forEach((config) => {
+        config.units.forEach((u) => {
+            validateConfig(u, false);
+            delete u.schema;
+        });
+        validateConfig(config, false);
+        delete config.schema;
+    });
+
+    if (debug) parsed.forEach((c) => console.log(`Creating method config: ${c.name}`));
+    return parsed;
+}
+
+// Build and write MODEL configs
 try {
     const modelAssetFiles = getAssetFiles(MODEL_ASSETS_PATH, ".yml");
     const modelConfigs = modelAssetFiles.flatMap((asset) => createModelConfigs(asset, true));
@@ -53,3 +70,11 @@ try {
     console.error(e);
 }
 
+// Build and write METHOD configs
+try {
+    const methodAssetFiles = getAssetFiles(METHOD_ASSETS_PATH, ".yml");
+    const methodConfigs = methodAssetFiles.flatMap((asset) => createMethodConfigs(asset, true));
+    fs.writeFileSync("./method_list.js", `module.exports = ${JSON.stringify(methodConfigs)}`, "utf8");
+} catch (e) {
+    console.error(e);
+}
