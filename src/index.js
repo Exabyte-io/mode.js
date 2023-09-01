0a771d3f755c30060a5a02868a4567504a75fb2d
Alexander Zech <alexzech@gmx.de>
2023-08-31 17:36:40 -0700

    chore: move data files to src/data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import * as context from "./context";
+import { allMethods as categorizedMethodList } from "./data/method_list";
+import { allModels as categorizedModelList } from "./data/model_list";
 import * as default_methods from "./default_methods";
 import * as default_models from "./default_models";
 import { Method } from "./method";
-import { allMethods as categorizedMethodList } from "./method_list";
 import { MethodFactory } from "./methods/factory";
 import { PseudopotentialMethod } from "./methods/pseudopotential";
 import { Model } from "./model";
-import { allModels as categorizedModelList } from "./model_list";
 import { DFTModel } from "./models/dft";
 import { ModelFactory } from "./models/factory";
 // TODO : can replace these with subpackages in exports in package.json
