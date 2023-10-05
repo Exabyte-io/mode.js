64bbac07c8f25b2de9da311e6160e9cd7e4e2f07
Alexander Zech <alexzech@gmx.de>
2023-10-04 19:36:45 -0700

    test: model factory for vasp dft model

--- tests/model.test.js
+++ tests/model.test.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 
 import { Model } from "../src/model";
+import { ModelFactory } from "../src/models/factory";
 
 describe("Model", () => {
     const obj = { type: "dft" };
@@ -8,5 +9,19 @@ describe("Model", () => {
     it("can be created", () => {
         const app = new Model(obj);
         expect(app.type).to.equal("dft");
+    });
+});
+
+const vaspModel = {
+    application: { name: "vasp", shortName: "vasp", version: "5.3.5" },
+    type: "dft",
+};
+
+describe("ModelFactory", () => {
+    it("should create a DFTModel for VASP with PAW method", () => {
+        const model = ModelFactory.create(vaspModel);
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        expect(model.method.subtype).to.be.equal("paw");
     });
 });
