a4e6620c16cd4acf057de6bd530b6aec551ee2f9
Alexander Zech <alexzech@gmx.de>
2023-10-24 18:34:42 -0700

    test: create dft model via factory

--- tests/dft_model.test.ts
+++ tests/dft_model.test.ts
@@ -1,11 +1,18 @@
 import { expect } from "chai";
 
 import { DFTModel } from "../src/models/dft";
+import { ModelFactory } from "../src/models/factory";
 
 describe("DFTModel", () => {
     it("should have a default functional", () => {
         const config = { type: "dft", subtype: "lda" };
         const model = new DFTModel(config);
         expect(model.defaultFunctional.slug).to.be.equal("pz");
+    });
+
+    it("can be created from the factory", () => {
+        const config = { type: "dft" };
+        const model = ModelFactory.create(config) as DFTModel;
+        expect(model.functional?.slug).to.be.equal("pbe");
     });
 });
