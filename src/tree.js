dce6f12856f9c0d9f24cf3d07b0550e1ad7e96e2
Alexander Zech <alexzech@gmx.de>
2023-10-25 19:20:21 -0700

    chore: re-implement tree utility functions

--- src/tree.js
+++ src/tree.js
@@ -1,4 +1,7 @@
 // TODO: migrate to use manifest instead
+import { MethodInterface } from "./utils/method_interface";
+import { ModelInterface } from "./utils/model_interface";
+import { safelyGetSlug } from "./utils/slugifiedEntry";
 
 export const METHODS = {
     pseudopotential: "pseudopotential",
@@ -14,3 +17,23 @@ export const MODEL_NAMES = {
     ml: "machine learning",
     re: "regression",
 };
+
+export function getAvailableDFTFunctionals() {
+    const functionals = ModelInterface.filterCategorizedModels().reduce((accumulator, item) => {
+        if (item.parameters?.functional) {
+            accumulator.push(item.parameters.functional);
+        }
+        return accumulator;
+    }, []);
+    return [...new Set(functionals)];
+}
+
+export function getPseudopotentialTypes() {
+    const subtypes = MethodInterface.filterLegacyMethods().reduce((accumulator, item) => {
+        if (item.type === METHODS.pseudopotential) {
+            accumulator.push(safelyGetSlug(item.subtype));
+        }
+        return accumulator;
+    }, []);
+    return [...new Set(subtypes)];
+}
