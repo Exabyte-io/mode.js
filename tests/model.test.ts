5c6b47e8df124c708f0624f66add828f17d3d9a8
Alexander Zech <alexzech@gmx.de>
2023-10-26 17:35:15 -0700

    test: default models for applications

--- tests/model.test.ts
+++ tests/model.test.ts
@@ -24,4 +24,38 @@ describe("Model", () => {
         expect(model.subtype).to.be.equal("unknown");
         expect(model._defaultCategorized).to.be.undefined;
     });
+
+    it("default model for espresso is DFT PBE", () => {
+        const application = { name: "espresso", version: "6.3", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        // @ts-ignore
+        expect(model.functional.slug).to.be.equal("pbe");
+    });
+
+    it("default model for vasp is DFT PBE", () => {
+        const application = { name: "vasp", version: "5.4.4", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        // @ts-ignore
+        expect(model.functional.slug).to.be.equal("pbe");
+    });
+
+    it("default model for nwchem is DFT B3LYP", () => {
+        const application = { name: "nwchem", version: "6.6", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("hybrid");
+        // @ts-ignore
+        expect(model.functional.slug).to.be.equal("b3lyp");
+    });
+
+    it("default model for exabyteml is regression", () => {
+        const application = { name: "exabyteml", version: "0.2.0", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("ml");
+        expect(model.subtype).to.be.equal("re");
+    });
 });
