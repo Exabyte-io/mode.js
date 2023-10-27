e21ba6ef78561bcd6accc2a6e9f6f72f8aa04b86
Alexander Zech <alexzech@gmx.de>
2023-10-27 10:30:05 -0700

    test: default dft model + group slug

--- tests/dft_model.test.ts
+++ tests/dft_model.test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 
+import { DFTModelConfig } from "../src/default_models";
 import { DFTModel } from "../src/models/dft";
 import { ModelFactory } from "../src/models/factory";
 
@@ -33,5 +34,25 @@ describe("DFTModel", () => {
         expect(model.functional?.slug).to.be.equal("pbe");
         expect(model._json.otherProperty).to.be.equal(42);
         expect(model.method.type).to.be.equal("pseudopotential");
+    });
+
+    it("can be created from default model config", () => {
+        const model = ModelFactory.create(DFTModelConfig);
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        // @ts-ignore
+        expect(model.functional.slug).to.be.equal("pbe");
+    });
+
+    it("creates the group slug 'qe:dft:gga:pbe' for DFT GGA", () => {
+        const application = {
+            name: "espresso",
+            shortName: "qe",
+            version: "6.3",
+            build: "Default",
+        };
+        const model = ModelFactory.createFromApplication({ application });
+        const expectedGroupSlug = "qe:dft:gga:pbe";
+        expect(model.groupSlug).to.be.equal(expectedGroupSlug);
     });
 });
