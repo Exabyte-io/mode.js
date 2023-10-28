25770b452f7ea08c15d3bb31d88587480fb4c3cb
Alexander Zech <alexzech@gmx.de>
2023-10-27 18:21:52 -0700

    test: update tests

---
+++ tests/dft_model.test.js
@@ -0,0 +1,64 @@
+import { expect } from "chai";
+
+import { DFTModelConfig } from "../src/default_models";
+import { DFTModel } from "../src/models/dft";
+import { ModelFactory } from "../src/models/factory";
+
+describe("DFTModel", () => {
+    it("should have a default functional", () => {
+        const config = { type: "dft", subtype: "lda" };
+        const model = new DFTModel(config);
+        expect(model.defaultFunctional.slug).to.be.equal("pz");
+    });
+
+    it("can be created from the factory", () => {
+        const config = { type: "dft", subtype: "lda", functional: "pz" };
+        const model = ModelFactory.create(config);
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("lda");
+        expect(model.functional?.slug).to.be.equal("pz");
+    });
+
+    it("can be created from the factory with incomplete config", () => {
+        const config = { type: "dft" };
+        const model = ModelFactory.create(config);
+        expect(model.functional?.slug).to.be.equal("pbe");
+    });
+
+    it("can be created from the factory given an application", () => {
+        const config = {
+            application: {
+                name: "espresso",
+                version: "6.3",
+                build: "Default",
+            },
+            otherProperty: 42,
+        };
+        const model = ModelFactory.createFromApplication(config);
+        expect(model).to.be.instanceof(DFTModel);
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        expect(model.functional?.slug).to.be.equal("pbe");
+        expect(model._json.otherProperty).to.be.equal(42);
+        expect(model.method.type).to.be.equal("pseudopotential");
+    });
+
+    it("can be created from default model config", () => {
+        const model = ModelFactory.create(DFTModelConfig);
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        expect(model.functional.slug).to.be.equal("pbe");
+    });
+
+    it("creates the group slug 'qe:dft:gga:pbe' for DFT GGA", () => {
+        const application = {
+            name: "espresso",
+            shortName: "qe",
+            version: "6.3",
+            build: "Default",
+        };
+        const model = ModelFactory.createFromApplication({ application });
+        const expectedGroupSlug = "qe:dft:gga:pbe";
+        expect(model.groupSlug).to.be.equal(expectedGroupSlug);
+    });
+});
--- tests/model.test.js
+++ tests/model.test.js
@@ -1,12 +1,57 @@
 import { expect } from "chai";
 
 import { Model } from "../src/model";
+import { ModelFactory } from "../src/models/factory";
 
 describe("Model", () => {
     const obj = { type: "dft" };
 
     it("can be created", () => {
-        const app = new Model(obj);
-        expect(app.type).to.equal("dft");
+        const model = new Model(obj);
+        expect(model.type).to.equal("dft");
+    });
+
+    it("can be created from the model factory", () => {
+        const model = ModelFactory.create(obj);
+        expect(model.type).to.be.equal("dft");
+    });
+
+    it("should have 'unknown' model as default if type is 'unknown'", () => {
+        const config = { type: "unknown" };
+        const model = ModelFactory.create(config);
+        expect(model.type).to.be.equal("unknown");
+        expect(model.subtype).to.be.equal("unknown");
+        expect(model._defaultCategorized).to.be.undefined;
+    });
+
+    it("default model for espresso is DFT PBE", () => {
+        const application = { name: "espresso", version: "6.3", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        expect(model.functional.slug).to.be.equal("pbe");
+    });
+
+    it("default model for vasp is DFT PBE", () => {
+        const application = { name: "vasp", version: "5.4.4", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("gga");
+        expect(model.functional.slug).to.be.equal("pbe");
+    });
+
+    it("default model for nwchem is DFT B3LYP", () => {
+        const application = { name: "nwchem", version: "6.6", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("dft");
+        expect(model.subtype).to.be.equal("hybrid");
+        expect(model.functional.slug).to.be.equal("b3lyp");
+    });
+
+    it("default model for exabyteml is regression", () => {
+        const application = { name: "exabyteml", version: "0.2.0", build: "Default" };
+        const model = ModelFactory.createFromApplication({ application });
+        expect(model.type).to.be.equal("ml");
+        expect(model.subtype).to.be.equal("re");
     });
 });
